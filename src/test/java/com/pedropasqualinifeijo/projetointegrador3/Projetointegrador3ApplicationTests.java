826e7e655c445d33938105479c9995aa2f77640c
unknown <pedropasqualini@live.com>
2025-02-01 01:15:22 -0300

    project start

---
+++ src/test/java/com/pedropasqualinifeijo/projetointegrador3/Projetointegrador3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedropasqualinifeijo.projetointegrador3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetointegrador3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
